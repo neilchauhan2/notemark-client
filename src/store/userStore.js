812e691ba180a92cc46a951a0f659cb15957c661
Neil Chauhan <neilchauhan2@gmail.com>
2020-05-10 19:23:21 +0530

    updated store with methods

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,3 +1,117 @@
 import { writable } from "svelte/store";
 
 export const user = writable({});
+
+export const isAuthenticated = writable(false);
+
+let token = localStorage.getItem("token")
+  ? localStorage.getItem("token")
+  : null;
+
+// ----------Auth Methods -->
+const tokenConfig = () => {
+  // Headers
+  const config = {
+    headers: {
+      "Content-type": "application/json"
+    }
+  };
+
+  // if token available add it to headers
+  if (token) {
+    config.headers["x-auth-token"] = token;
+  }
+
+  return config;
+};
+
+// signup method
+export const signup = async (credentials) => {
+  try {
+    document.getElementById("signup-btn").classList.add("is-loading");
+    const res = await axios.post(
+      "https://notemark.herokuapp.com/api/user/signup",
+      {
+        ...credentials
+      }
+    );
+    document.getElementById("signup-btn").classList.remove("is-loading");
+    // updated
+    user.update((user) => {
+      return {
+        ...res.data,
+        ...user
+      };
+    });
+    localStorage.setItem("token", res.data.token);
+    isAuthenticated.update((n) => {
+      return true;
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+// login method
+export const login = async (credentials) => {
+  try {
+    document.getElementById("login-btn").classList.add("is-loading");
+    const res = await axios.post(
+      "https://notemark.herokuapp.com/api/user/login",
+      {
+        ...credentials
+      }
+    );
+
+    localStorage.setItem("token", res.data.token);
+    // updated
+    user.update((user) => {
+      return {
+        ...res.data,
+        ...user
+      };
+    });
+    isAuthenticated.update((n) => {
+      return true;
+    });
+    document.getElementById("login-btn").classList.remove("is-loading");
+    navigate("/");
+    getAllBookmarks();
+    getAllNotes();
+  } catch (error) {
+    throw error;
+  }
+};
+
+// get user method
+export const loadUser = async () => {
+  try {
+    const config = tokenConfig();
+    const res = await axios.get(
+      "https://notemark.herokuapp.com/api/user/",
+      config
+    );
+    user.update((user) => {
+      return {
+        ...res.data,
+        ...user
+      };
+    });
+    isAuthenticated.update((n) => {
+      return true;
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+// logout method
+export const logout = () => {
+  localStorage.removeItem("token");
+  isAuthenticated.update((n) => {
+    return false;
+  });
+  user.update((user) => {
+    return {};
+  });
+};
